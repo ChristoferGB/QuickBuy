720e70332df101466916e35838a10942216c3a46
ChristoferGB <31290896+ChristoferGB@users.noreply.github.com>
2020-07-14 16:37:13 -0300

    Mudanças na página de login e bindings

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core"
+import { Usuario } from "../../model/usuario";
 
 @Component({
   selector: "app-login",
@@ -6,5 +7,15 @@ import { Component } from "@angular/core"
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
+
+  public usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    alert(this.usuario.email + " - " + this.usuario.senha);
+  }
 
 }
